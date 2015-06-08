67e447103e8e7559192a65987054f15b2eef85ee
bigmstone <matthew99@gmail.com>
2015-06-07 20:30:45 -0500

    Started work on test comments. Added tox file.
    Change-Id: I7b5be81b7e418ee970c43608977773cc0e1d8961

--- tests/netconf/test_bgp.py
+++ tests/netconf/test_bgp.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
-'''
-BGP Unittest
-'''
+"""
+Copyright 2015 Brocade Communications Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 import unittest
 from pynos.netconf.bgp import BGP
 import xml.etree.ElementTree as ET
 
 class _Tester(object):
-    '''
-    Tester object
-    '''
+    """
+    Tester class 
+    """
     def __init__(self):
         self.bgp = BGP(self._callback)
         self.output = None
