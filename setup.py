56127da8ec0cb7955d2e34f0a83c40c964db6827
bigmstone <matthew99@gmail.com>
2015-07-20 22:46:10 -0500

    rev ver 1.0
    Change-Id: I779bb26f831c44604389a1edfdb82c7b45ceb56e

--- setup.py
+++ setup.py
@@ -5,19 +5,18 @@ Setup for pynos
 from setuptools import setup, find_packages
 
 setup(name='pynos',
-      version='0.9.0',
+      version='1.0.0',
       description='Brocade NOS Library.',
       author='Brocade Communications, LLC',
       author_email='mstone@brocade.com',
       url='http://www.brocade.com/',
       packages=find_packages(),
       classifiers=[
-          'Development Status :: 4 - Beta',
+          'Development Status :: 5 - Production/Stable',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: Apache Software License',
           'Operating System :: OS Independent',
           'Programming Language :: Python',
-          'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
           'Topic :: Software Development :: Libraries',
           'Topic :: Software Development :: Libraries :: Python Modules',
