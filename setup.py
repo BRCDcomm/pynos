3b25aa847e3a356d5fb39a3187119b52caacf474
Lindsay Hill <lindsay.k.hill@gmail.com>
2015-07-23 15:46:08 +1200

    [PYNOS-51] Fix company name
    Change-Id: I166acbc928f2a979ec5793de5e74c0bbc0ba078e

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 setup(name='pynos',
       version='1.0.0',
       description='Brocade NOS Library.',
-      author='Brocade Communications, LLC',
+      author='Brocade Communications Systems, Inc.',
       author_email='mstone@brocade.com',
       url='http://www.brocade.com/',
       packages=find_packages(),
