4a913bddfa45a33a18fcc520a6d92b411287a188
bigmstone <matthew99@gmail.com>
2015-08-07 20:02:06 -0500

    rev version
    Change-Id: I74a000962f467c393f93dddf8763ff97d1a8359d

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Setup for pynos
 from setuptools import setup, find_packages
 
 setup(name='pynos',
-      version='1.0.1',
+      version='1.1.0',
       description='Brocade NOS Library.',
       author='Brocade Communications Systems, Inc.',
       author_email='mstone@brocade.com',
