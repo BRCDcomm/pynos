e802e73b58821d735033b460c4d97b85723ba424
Tyler Christiansen <code@tylerc.me>
2016-02-08 19:55:40 -0800

    Bump version to 1.3.1
    Change-Id: I712d82d0af67f6fa80a82ccde54eb9a0326a9acb

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Setup for pynos
 from setuptools import setup, find_packages
 
 setup(name='pynos',
-      version='1.3.0',
+      version='1.3.1',
       description='Brocade NOS Library.',
       author='Brocade Communications Systems, Inc.',
       author_email='mstone@brocade.com',
