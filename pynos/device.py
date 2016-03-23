3afa23390af8f6547c37b94286239405c750c34c
Lindsay Hill <lindsay.k.hill@gmail.com>
2016-03-24 11:22:12 +1300

    [PYNOS-118] Added support for NOS 6.0.2
    Change-Id: I269f194788fb0d47ec04aa73078a826a94e3497d

--- pynos/device.py
+++ pynos/device.py
@@ -65,6 +65,17 @@ VERSIONS = {
                           .FabricService,
         'vcs': pynos.versions.ver_6.ver_6_0_1.vcs.VCS,
     },
+    '6.0.2': {
+        'bgp': pynos.versions.ver_6.ver_6_0_1.bgp.BGP,
+        'snmp': pynos.versions.ver_6.ver_6_0_1.snmp.SNMP,
+        'interface': pynos.versions.ver_6.ver_6_0_1.interface.Interface,
+        'lldp': pynos.versions.ver_6.ver_6_0_1.lldp.LLDP,
+        'system': pynos.versions.ver_6.ver_6_0_1.system.System,
+        'services': pynos.versions.ver_6.ver_6_0_1.services.Services,
+        'fabric_service': pynos.versions.ver_6.ver_6_0_1.fabric_service
+                          .FabricService,
+        'vcs': pynos.versions.ver_6.ver_6_0_1.vcs.VCS,
+    },
     '7.0.0': {
         'bgp': pynos.versions.ver_7.ver_7_0_0.bgp.BGP,
         'snmp': pynos.versions.ver_6.ver_6_0_1.snmp.SNMP,
